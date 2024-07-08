2608e5530a2f2024fa0e77c76dee54b235e2d3fb
Vung Nguyen <vungnguyenn1001@gmail.com>
2024-07-09 01:39:31 +0700

    initial Youtube UI

---
+++ flutter_youtube/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    return MaterialApp(
+      title: 'Flutter Youtube UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        bottomNavigationBarTheme:
+            const BottomNavigationBarThemeData(selectedItemColor: Colors.white),
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
